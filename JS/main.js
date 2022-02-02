1cd35272a7367a7313e2a3e312779e2e4eaadd18
FLORBELA FREITAS OLIVEIRA <21276327@CFP127ALUNO.LOCAL>
2022-02-02 16:47:45 -0300

    código - 02/02

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,35 @@
 'use strict';
 
-function camposValidos(){
-    return document.getElementById('formulario').reportValidity();
-}
+import {iniciarRange} from "./input-range.js";
 
-function calcularImc(altura, peso) {
+const camposValidos = () => document.getElementById('formulario').reportValidity();
+
+/*function calcularImc(altura, peso) {
     return peso / (altura * altura);
-}
+}*/
+//Mudando de sintaxe
+const calcularImc = (altura, peso) => peso / (altura * altura);
 
-function classificarImc(imc) {
+//Classificando IMC
+const classificarImc = imc => {
     let texto;
     if(imc < 18.5) {
-        texto = "e você está abaixo do peso"
+        texto = "e você está abaixo do peso."
     } else if(imc < 25) {
-        texto = "e você está com peso ideal. Parabéns!"
+        texto = "e você está com peso ideal. <span id='parabens'>Parabéns!</span>"
     } else if(imc < 30) {
-        texto = "e você está levemente acima do peso"
+        texto = "e você está levemente acima do peso."
     } else if(imc < 35) {
-        texto = "e você está com obesidade grau I"
+        texto = "e você está com obesidade grau I."
     } else if(imc < 40) {
-        texto = "e você está com obesidade grau II"
+        texto = "e você está com obesidade grau II."
     } else {
-        texto = "e você está com obesidade grau III. Cuidado!"
+        texto = "e você está com obesidade grau III. <span id='cuidado'>Cuidado!</span>"
     }
     return texto;
 }
 
-function mostrarResultado () {
+const mostrarResultado = () => {
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value; 
     const peso = document.getElementById('peso').value;
@@ -36,9 +39,13 @@ function mostrarResultado () {
         const imc = calcularImc(altura, peso);
         let texto = classificarImc(imc);
         
-        resultado.textContent = `${nome} seu IMC é ${imc.toFixed(2)} ${texto}`; //anexando variáveis na String 
+        resultado.innerHTML = `${nome} seu IMC é ${imc.toFixed(2)} ${texto}`; //anexando variáveis na String 
     } else {
-        resultado.textContent = "Preencha todos os campos!";
+        resultado.innerHTML = "Preencha todos os campos!";
     }
 }
+
 document.getElementById('calcular').addEventListener('click', mostrarResultado);
+
+iniciarRange('altura')
+iniciarRange('peso')
